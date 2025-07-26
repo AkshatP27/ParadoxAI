f594bd42eb737441171fe3abbcb4985566dd6802
Akshat Panchal <akshat.coder.04@gmail.com>
2025-07-26 15:32:50 +0530

    Developed Header for large screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-9xl font-bold">Hello React...!</h1>
-    </div>
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
   );
 };
 
